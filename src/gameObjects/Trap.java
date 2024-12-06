97a240e08558ba1fe99e6dc10154ec209b2770b4
vitorspequito <vitorspequito@gmail.com>
2024-12-06 14:37:11 +0000

    fix: trap damage

--- src/gameObjects/Trap.java
+++ src/gameObjects/Trap.java
@@ -5,7 +5,7 @@ import utils.Point2D;
 public class Trap extends GameObject {
 	private static final String NAME = "Trap";
 	private static final int LAYER = 0;
-	private static final int DAMAGE = 200;
+	private static final int DAMAGE = 20;
 
 	public Trap(Point2D startingPosition) {
 		super(NAME, startingPosition, LAYER);
